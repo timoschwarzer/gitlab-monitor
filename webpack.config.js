f9ec9fc7fb1fe59b43e87d7fdc81909eddb48bbc
Timo Schwarzer <me@timoschwarzer.com>
2018-01-13 15:04:47 +0100

    Fix webpack publicPath when building in release mode

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map';
-  module.exports.output.publicPath = '/dashboard/';
+  module.exports.output.publicPath = '';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
