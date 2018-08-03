636327a503ae8d66797081aa579b3f8ca6679a73
Daniel Pozzi <bonndan76@googlemail.com>
2018-08-03 13:42:09 +0200

    Works in other context than root
    I seemingly have submitted a change to the wrong file today. Is this the right one to fix? Please check, thanks.

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: './dist/',
     filename: 'build.js'
   },
   module: {
