timoschwarzer/gitlab-monitor

commit 5ffd144212f975b6985dc8a54d52d4bc5a387632

Fix filtering by project tags (#120)
The previous logic selects all projects without tags even if an includeTags-filter is defined. If no tags are defined, the project should not be selected if a filter is defined.

src/components/app.vue
@@ -152,7 +152,7 @@
                     project.tag_list.length > 0 ||
                     !Config.root.filter.excludeUntagged
                   ) && (
-                    project.tag_list.length === 0 ||
+                    !Config.root.filter.includeTags) ||
                     project.tag_list.some(tag => tag.match(new RegExp(Config.root.filter.includeTags)))
                   )
                 )

commit 99479b6aa52fa23c1388f452f37907e104bdae90

Fix typo introduced in #120

src/components/app.vue
@@ -152,7 +152,7 @@
                     project.tag_list.length > 0 ||
                     !Config.root.filter.excludeUntagged
                   ) && (
-                    !Config.root.filter.includeTags) ||
+                    !Config.root.filter.includeTags ||
                     project.tag_list.some(tag => tag.match(new RegExp(Config.root.filter.includeTags)))
                   )
                 )