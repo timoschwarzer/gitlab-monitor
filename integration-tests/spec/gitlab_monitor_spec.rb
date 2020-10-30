2ff87c1e6c94cab5292f1d45304679369769aee3
Dave Hytinen <david.hytinen@ithaka.org>
2020-10-30 14:07:28 -0400

    un-wip test

--- integration-tests/spec/gitlab_monitor_spec.rb
+++ integration-tests/spec/gitlab_monitor_spec.rb
@@ -2,7 +2,7 @@ require_relative './support/matchers'
 require_relative '../lib/gitlab_monitor'
 include ITHAKA
 
-describe "Gitlab Monitor", :wip, :pow1472 do
+describe "Gitlab Monitor" do
   describe "healthcheck" do
     it 'is up' do
       expect(GitlabMonitor.status.code).to eq('200')
