5b6e77f20434d0c78d1edae6c89639d38d56c035
Dave Hytinen <david.hytinen@ithaka.org>
2020-10-30 14:12:14 -0400

    update healthcheck path

--- integration-tests/lib/gitlab_monitor.rb
+++ integration-tests/lib/gitlab_monitor.rb
@@ -5,7 +5,7 @@ module GitlabMonitor
   @service_url = ITHAKA::ServiceLocation.get_host(APP_NAME)
 
   def status
-    call_params = {method: 'get', host: @service_url, path: '/healthcheck'}
+    call_params = {method: 'get', host: @service_url, path: '/'}
     @rest.call(call_params)
   end
 end
