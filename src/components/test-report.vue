fe29d5248bc569c5f748342aa2067f05a7bb9478
GabrielOliveira <gabriel.p.a.deoliveira@quby.com>
2021-01-29 14:56:31 +0100

    Add icons to test-report and adjust css a bit

--- src/components/test-report.vue
+++ src/components/test-report.vue
@@ -7,11 +7,17 @@
             rel="noopener noreferrer"
             :href="pipeline.web_url + '/test_report'"
         >
-          <span class="test-summary">Total ({{ pipeline.test_report.total_count }})</span>
-          <span class="test-summary">Sucess ({{ pipeline.test_report.success_count }})</span>
-          <span class="test-summary">Failed ({{ pipeline.test_report.failed_count }})</span>
-          <span class="test-summary">Skipped ({{ pipeline.test_report.skipped_count }})</span>
-          <span class="test-summary">Error ({{ pipeline.test_report.error_count }})</span>
+          <span class="test-summary">Test Report:</span>
+          <gitlab-icon class="test-icon" name="monitor" size="12" />
+          <span class="test-summary">{{ pipeline.test_report.total_count }}</span>
+          <gitlab-icon v-if="pipeline.test_report.success_count > 0" class="test-icon" name="status-success" size="12" />
+          <span v-if="pipeline.test_report.success_count > 0" class="test-summary">{{ pipeline.test_report.success_count }}</span>
+          <gitlab-icon v-if="pipeline.test_report.failed_count > 0" class="test-icon" name="status-failed" size="12" />
+          <span v-if="pipeline.test_report.failed_count > 0" class="test-summary">{{ pipeline.test_report.failed_count }}</span>
+          <gitlab-icon v-if="pipeline.test_report.skipped_count > 0" class="test-icon" name="status_skipped_borderless" size="12" />
+          <span v-if="pipeline.test_report.skipped_count > 0" class="test-summary">{{ pipeline.test_report.skipped_count }}</span>
+          <gitlab-icon v-if="pipeline.test_report.error_count > 0" class="test-icon" name="error" size="12" />
+          <span v-if="pipeline.test_report.error_count > 0" class="test-summary">{{ pipeline.test_report.error_count }}</span>
         </a>
     </div>
   </div>
@@ -20,8 +26,12 @@
 
 <script>
 import Config from '../Config'
+import GitlabIcon from './gitlab-icon'
 
 export default {
+    components: {
+      GitlabIcon
+    },
     name: 'test-report',
     props: ['pipeline'],
     computed: {
@@ -35,7 +45,6 @@ export default {
 <style lang="scss" scoped>
     .test-report {
       padding: 1px;
-      border-top: 1px solid rgba(255, 255, 255, 0.1);
       display: flex;
       align-items: center;
       color: white;
@@ -43,6 +52,11 @@ export default {
       
       a {
         text-decoration: none;
+      }
+
+      .test-icon {
+        margin-right: 3px;
+        color: var(--pipeline-chart-icon, rgba(255, 255, 255, 0.5));
       }
 
       .test-summary {
