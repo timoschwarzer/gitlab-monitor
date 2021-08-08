14f65a9b6dd1eac4ed0ca7534c88b0f6d0ad1431
Timo Schwarzer <timo@schwarzer.dev>
2021-08-08 19:23:01 +0200

    Improve runner status

--- src/components/runner-status.vue
+++ src/components/runner-status.vue
@@ -1,115 +1,117 @@
 <template>
   <div>
-    <div class="container" v-if="(runners_online.length + runners_active.length + runners_paused.length + runners_offline.length) > 0">
-      <span class="entry">Runner status:</span>
-      <span class="entry" v-if="runners_online.length > 0" :title="runners_online.join(', ')">
-        <div class="icon-online"/>
-        {{ runners_online.length }} online
-      </span>
-      <span class="entry" v-if="runners_active.length > 0" :title="runners_active.join(', ')">
-        <div class="icon-active"/>
-        {{ runners_active.length }} active
-      </span>
-      <span class="entry" v-if="runners_paused.length > 0" :title="runners_paused.join(', ')">
-        <div class="icon-paused"/>
-        {{ runners_paused.length}} paused
-      </span>
-      <span class="entry" v-if="runners_offline.length > 0" :title="runners_offline.join(', ')">
-        <div class="icon-offline"/>
-        {{ runners_offline.length}} offline
-      </span>
+    <div
+      class="container"
+      v-if="(runnersOnline.length + runnersActive.length + runnersPaused.length + runnersOffline.length) > 0"
+    >
+      <div class="entry">Runner status:</div>
+      <div class="entry" v-if="runnersOnline.length > 0" :title="runnersOnline.join(', ')">
+        <div class="icon icon-online"/>
+        {{ runnersOnline.length }} online
+      </div>
+      <div class="entry" v-if="runnersActive.length > 0" :title="runnersActive.join(', ')">
+        <div class="icon icon-active"/>
+        {{ runnersActive.length }} active
+      </div>
+      <div class="entry" v-if="runnersPaused.length > 0" :title="runnersPaused.join(', ')">
+        <div class="icon icon-paused"/>
+        {{ runnersPaused.length }} paused
+      </div>
+      <div class="entry" v-if="runnersOffline.length > 0" :title="runnersOffline.join(', ')">
+        <div class="icon icon-offline"/>
+        {{ runnersOffline.length }} offline
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "runner-status",
-  props: [],
-  data: () => ({
-    runners_online: [],
-    runners_active: [],
-    runners_paused: [],
-    runners_offline: [],
-    refreshIntervalId: null
-  }),
-  computed: {},
-  mounted() {
-    this.fetchRunners()
-    this.setupRefreshInterval()
-  },
-  beforeDestroy() {
-    if (this.refreshIntervalId) {
-      clearInterval(this.refreshIntervalId)
-    }
-  },
-  methods: {
-    async fetchRunners() {
-      const runners = await this.$api('/runners')
-      this.runners_online = runners.filter(runner => runner.status === 'online').map(runner => runner.description)
-      this.runners_active = runners.filter(runner => runner.status === 'active').map(runner => runner.description)
-      this.runners_offline = runners.filter(runner => runner.status === 'offline').map(runner => runner.description)
-      this.runners_paused = runners.filter(runner => runner.status === 'paused').map(runner => runner.description)
+  export default {
+    name: 'runner-status',
+    props: [],
+    data: () => ({
+      runnersOnline: [],
+      runnersActive: [],
+      runnersPaused: [],
+      runnersOffline: [],
+      refreshIntervalId: null,
+      fetchAllRunners: true,
+    }),
+    computed: {},
+    mounted() {
+      this.fetchRunners()
+      this.setupRefreshInterval()
     },
-    setupRefreshInterval() {
+    beforeDestroy() {
       if (this.refreshIntervalId) {
-        clearInterval((this.refreshIntervalId))
+        clearInterval(this.refreshIntervalId)
       }
-      this.refreshIntervalId = setInterval(this.fetchRunners, 10000)
+    },
+    methods: {
+      async fetchRunners() {
+        const runners = await this.$api('/runners')
+
+        try {
+          const allRunners = await this.$api('/runners/all')
+
+          for (const runner of allRunners) {
+            if (!runners.some(r => r.id === runner.id)) {
+              runners.push(runner)
+            }
+          }
+        } catch (e) {
+          this.fetchAllRunners = false
+          console.warn('Could not fetch all instance runners and won\'t try again.')
+        }
+
+        this.runnersOnline = runners.filter(runner => runner.status === 'online').map(runner => runner.description)
+        this.runnersActive = runners.filter(runner => runner.status === 'active').map(runner => runner.description)
+        this.runnersOffline = runners.filter(runner => runner.status === 'offline').map(runner => runner.description)
+        this.runnersPaused = runners.filter(runner => runner.status === 'paused').map(runner => runner.description)
+      },
+      setupRefreshInterval() {
+        if (this.refreshIntervalId) {
+          clearInterval((this.refreshIntervalId))
+        }
+        this.refreshIntervalId = setInterval(this.fetchRunners, 10000)
+      }
     }
   }
-
-}
 </script>
 
 <style lang="scss" scoped>
-.container {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  background-color: var(--background-color, rgba(255,255,255,0.5));
-}
+  .container {
+    background-color: var(--background-color, rgba(255, 255, 255, 0.5));
+    padding: 4px 8px;
+  }
 
-.entry {
-  margin-left: 15px;
-  margin-right: 15px;
-}
+  .entry {
+    display: inline-block;
+    margin-right: 1em;
+  }
 
-.icon-online {
-  display: inline-flex;
-  width: 10px;
-  height: 10px;
-  background-color: #2E7D32;
-  border: 2px solid rgba(0, 0, 0, 0.2);
-  border-radius: 999px;
-}
+  .icon {
+    display: inline-block;
+    width: 0.8em;
+    height: 0.8em;
+    transform: translateY(-10%);
+    vertical-align: middle;
+    border-radius: 50%;
 
-.icon-active {
-  display: inline-flex;
-  width: 10px;
-  height: 10px;
-  background-color: #1565C0;
-  border: 2px solid rgba(0, 0, 0, 0.2);
-  border-radius: 999px;
-}
+    &.icon-online {
+      background-color: var(--job-success, #2E7D32);
+    }
 
-.icon-paused {
-  display: inline-flex;
-  width: 10px;
-  height: 10px;
-  background-color: #EF6C00;
-  border: 2px solid rgba(0, 0, 0, 0.2);
-  border-radius: 999px;
-}
+    &.icon-active {
+      background-color: var(--job-running, #1565C0);
+    }
 
-.icon-offline {
-  display: inline-flex;
-  width: 10px;
-  height: 10px;
-  background-color: #C62828;
-  border: 2px solid rgba(0, 0, 0, 0.2);
-  border-radius: 999px;
-}
+    &.icon-paused {
+      background-color: var(--job-pending, #EF6C00);
+    }
 
+    &.icon-offline {
+      background-color: var(--job-failed, #C62828);
+    }
+  }
 </style>
